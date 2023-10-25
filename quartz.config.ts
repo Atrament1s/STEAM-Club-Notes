58eb6a90ad43b55fabf4a9899df167a0b747dad2
Atrament1s <93157646+Atrament1s@users.noreply.github.com>
2023-10-24 23:01:15 -0500

    Quartz sync: Oct 24, 2023, 11:01 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "atrament1s.github.io/STEAM-Club-Notes",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
